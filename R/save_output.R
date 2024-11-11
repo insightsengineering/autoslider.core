c1c9072e07e6fa827c245c90f9cc44085c1dce47
shajoezhu <sha.joe.zhu@gmail.com>
2024-11-11 11:45:42 +0800

    updating, adding functions

---
+++ R/save_output.R
@@ -0,0 +1,188 @@
+#' Save an Output
+#'
+#' @param output Output object, e.g. an `rtable` or `grob`
+#' @param file_name Full path of the new file *excluding* the extension
+#' @param save_rds Saved as an `.rds` files
+#' @details
+#' Tables are saved as RDS file
+#'
+#' @return The input `object` invisibly
+#' @export
+#'
+#' @examples
+#' library(dplyr)
+#' adsl <- eg_adsl %>%
+#'   mutate(TRT01A = factor(TRT01A, levels = c("A: Drug X", "B: Placebo")))
+#' adae <- eg_adae %>%
+#'   mutate(TRT01A = factor(TRT01A, levels = c("A: Drug X", "B: Placebo")))
+#' adae$ATOXGR <- adae$AETOXGR
+#' output_dir <- tempdir()
+#' t_ae_pt_slide(adsl, adae, "TRT01A", 2) %>%
+#'   decorate(
+#'     title = "Serious Adverse Events, Safety-Evaluable Patients",
+#'     footnote = ""
+#'   ) %>%
+#'   save_output(
+#'     file_name = file.path(output_dir, "t_ae_SER_SE"),
+#'     save_rds = TRUE
+#'   )
+#'
+setGeneric("save_output", function(output, file_name, save_rds) {
+  standardGeneric("save_output")
+})
+
+#' @rdname save_output
+save_output <- function(output, file_name, save_rds = TRUE) {
+  UseMethod("save_output")
+}
+
+#' @rdname save_output
+#' @export
+save_output.autoslider_error <- function(output,
+                                         file_name,
+                                         save_rds = TRUE) {
+  output
+}
+
+#' @rdname save_output
+#' @aliases save_output, dVTableTree, dVTableTree-method
+setMethod("save_output", "dVTableTree", save_output.dVTableTree <- function(output, file_name, save_rds = TRUE) {
+  if (save_rds) {
+    saveRDS(output, file = paste0(file_name, ".rds"))
+  }
+
+  invisible(output)
+})
+
+#' @rdname save_output
+#' @export
+save_output.decoratedGrob <- function(output,
+                                      file_name,
+                                      save_rds = TRUE) {
+  if (save_rds) {
+    saveRDS(output, file = paste0(file_name, ".rds"))
+  }
+
+  invisible(output)
+}
+
+#' @rdname save_output
+#' @export
+save_output.decoratedGrobSet <- function(output, file_name, save_rds = TRUE) {
+  if (save_rds) {
+    saveRDS(output, file = paste0(file_name, ".rds"))
+  }
+
+  invisible(output)
+}
+
+
+#' @rdname save_output
+#' @export
+save_output.dlisting <- function(output, file_name, save_rds = TRUE) {
+  if (save_rds) {
+    saveRDS(output, file = paste0(file_name, ".rds"))
+  }
+
+  invisible(output)
+}
+
+
+#' Save a list of outputs
+#'
+#' @param outputs `list` of outputs as created by `generate_outputs`
+#' @param outfolder Folder in which to store the `outputs``
+#' @param verbose_level Level of verbose information displayed.
+#'        Default set to `1`.
+#' @param save_rds Should the input `outputs` be saved as `.rds` files in
+#'        in addition to `.out` or `.pdf` files? Defaults to `FALSE`.
+#' @param generic_suffix generic suffix. must be length 1 character or NULL.
+#' @export
+#'
+#' @examples
+#' ## As `save_outputs` is the last step in the pipeline we have to run
+#' ## the 'whole machinery' in order to show its functionality. Also take a look
+#' ## at the `AutoslideR-Demo` repo on code.roche.com.
+#' library(dplyr, warn.conflicts = FALSE)
+#'
+#' data <- list(
+#'   adsl = eg_adsl,
+#'   adae = eg_adae,
+#'   adtte = eg_adtte
+#' )
+#'
+#' filters::load_filters(
+#'   yaml_file = system.file("filters.yml", package = "autoslideR"),
+#'   overwrite = TRUE
+#' )
+#'
+#' ## For this example the outputs will be saved in a temporary directory. In a
+#' ## production run this should be the reporting event's 'output' folder instead.
+#' output_dir <- tempdir()
+#'
+#' spec_file <- system.file("spec.yml", package = "autoslideR")
+#' read_spec(spec_file) %>%
+#'   filter_spec(program == "t_dm_slide") %>%
+#'   generate_outputs(datasets = data) %>%
+#'   decorate_outputs() %>%
+#'   save_outputs(outfolder = output_dir)
+#'
+save_outputs <- function(outputs,
+                         outfolder = file.path("output"),
+                         generic_suffix = NULL,
+                         save_rds = TRUE,
+                         verbose_level = 1) {
+  stopifnot(is.list(outputs))
+
+  if (!dir.exists(outfolder)) {
+    dir.create(outfolder)
+  }
+  if (!is.null(generic_suffix)) {
+    if (!(is.character(generic_suffix) & length(generic_suffix) == 1)) {
+      stop("generic suffix must be length 1 character!")
+    }
+  }
+  ret <- lapply(outputs, function(output) {
+    spec <- attr(output, "spec")
+    file_path <- file.path(outfolder, spec$output)
+    file_path <- paste0(c(file_path, generic_suffix), collapse = "_")
+    output <- save_output(
+      output = output,
+      file_name = file_path,
+      save_rds = save_rds
+    )
+
+    if (verbose_level > 0) {
+      if (is(output, "autoslider_error")) {
+        cat_bullet(
+          "Saving output ",
+          attr(output, "spec")$output,
+          " failed in step ",
+          attr(output, "step"),
+          " with error message: ",
+          toString(output),
+          bullet = "cross",
+          bullet_col = "red"
+        )
+      } else {
+        cat_bullet(
+          "Output saved in path ",
+          file_path,
+          bullet = "tick",
+          bullet_col = "green"
+        )
+      }
+    }
+
+    attr(output, "outpath") <- get_output_file_ext(output, file_path)
+    output
+  })
+
+  if (verbose_level > 0) {
+    total_number <- length(ret)
+    fail_number <- sum(map_lgl(ret, is, class2 = "autoslider_error"))
+    log_success_infomation(total_number - fail_number, fail_number)
+  }
+
+  ret
+}
---
+++ R/spec.R
@@ -0,0 +1,159 @@
+#' Read yaml spec file
+#'
+#' Read yaml spec file and split according to filter lists
+#'
+#' @param spec_file `character`. Path to a yaml spec file
+#' @param metadata Metadata of study
+#'
+#' @return
+#' An object of class `spec` which is a `list` where each element corresponds
+#' to one output, e.g. `t_dm_IT`.
+#'
+#' @author
+#' - Liming Li (`Lil128`)
+#' - Thomas Neitmann (`neitmant`)
+#'
+#' @export
+#'
+#' @examples
+#' spec_file <- system.file("spec.yml", package = "autoslideR")
+#'
+#' ## Take a look at the 'raw' content of the spec file
+#' cat(readLines(spec_file)[1:24], sep = "\n")
+#'
+#' ## This is how it looks once read into R
+#' spec <- read_spec(spec_file)
+#' spec[1:3]
+#'
+read_spec <- function(spec_file = "spec.yml",
+                      metadata = NULL) {
+  spec <- yaml::read_yaml(spec_file, eval.expr = TRUE)
+  ret <- lapply(spec, function(s) {
+    lapply(s$suffix, function(su) {
+      ret <- s
+      ret$suffix <- su
+      c(ret, metadata)
+    })
+  })
+  spec_obj <- unlist(ret, recursive = FALSE)
+  as_spec(spec_obj)
+}
+
+#' validate spec file
+#' @description not implemented yet
+#' @param spec specification
+#' @noRd
+validate_spec <- function(spec) {
+  message <- NULL
+  if (is.null(spec$dataset)) {
+    message <- c(message, "Spec must not assign dataset argument!")
+  }
+  if (is.null(spec$func)) {
+    message <- c(message, "Spec must include func argument!")
+  }
+  if (is.null(spec$outpath)) {
+    message <- c(message, "Spec must include outpath argument!")
+  }
+}
+
+#' Filter a spec object
+#'
+#' @param spec A `spec` object as returned by `read_spec()`
+#' @param filter_expr A `logical` expression indicating outputs to keep
+#' @param verbose Should a message about the number of outputs matching
+#'        `filter_spec` be printed? Defaults to `TRUE`.
+#'
+#' @return
+#' A `spec` object containing only the outputs matching `filter_expr`
+#'
+#' @author Thomas Neitmann (`neitmant`)
+#'
+#' @export
+#'
+#' @examples
+#' spec_file <- system.file("spec.yml", package = "autoslideR")
+#' spec <- spec_file %>% read_spec()
+#' ## Keep only outputs belonging to batch 2
+#' \dontrun{
+#' filter_spec(spec, batch == 2)
+#' }
+#'
+#' ## Keep only the t_dm_IT output
+#' filter_spec(spec, output == "t_dm_IT")
+#'
+#' ## Same as above but more verbose
+#' filter_spec(spec, program == "t_dm" && suffix == "IT")
+#'
+#' ## Keep all t_ae outputs
+#' filter_spec(spec, program == "t_ae")
+#'
+#' ## Keep all output run on safety population
+#' filter_spec(spec, "SE" %in% suffix)
+#'
+#' ## Keep t_dm_CHN_IT and t_dm_CHN_SE
+#' filter_spec(spec, program == "t_dm" && suffix %in% c("CHN_IT", "CHN_SE"))
+#'
+#' ## Keep all tables
+#' filter_spec(spec, grepl("^t_", program))
+#'
+filter_spec <- function(spec, filter_expr, verbose = TRUE) {
+  if (is.character(substitute(filter_expr))) {
+    warn_about_legacy_filtering(filter_expr)
+    condition <- bquote(output == .(filter_expr))
+  } else {
+    condition <- substitute(filter_expr)
+  }
+  stopifnot(is_spec(spec), is.language(condition), is.logical(verbose))
+  vars <- all.vars(condition)
+
+  filtered_spec <- Filter(function(output) {
+    assert_exists_in_spec_or_calling_env(vars, output)
+    p <- eval(condition, envir = output)
+    assert_is_valid_filter_result(p)
+    p
+  }, spec)
+
+  if (verbose) {
+    log_number_of_matched_records(spec, filtered_spec, condition)
+  }
+
+  as_spec(filtered_spec)
+}
+
+is_spec <- function(x) {
+  "spec" %in% class(x)
+}
+
+as_spec <- function(x) {
+  spec <- lapply(x, function(elem) {
+    if (is.null(elem$suffix)) {
+      elem$suffix <- ""
+    }
+
+    if (elem$suffix == "") {
+      elem$output <- elem$program
+    } else {
+      elem$output <- paste(elem$program, elem$suffix, sep = "_")
+    }
+
+    if (is.null(elem$paper)) {
+      elem$paper <- default_paper_size(elem$program)
+    } else if (elem$paper == "a4r") {
+      warn_about_legacy_paper_size("a4r", "L11")
+      elem$paper <- "L11"
+    } else if (elem$paper == "a4") {
+      warn_about_legacy_paper_size("a4", "P11")
+      elem$paper <- "P11"
+    } else {
+      validate_paper_size(elem$paper)
+    }
+
+    elem
+  })
+
+  structure(
+    .Data = spec,
+    names = map_chr(spec, `[[`, "output"),
+    class = union("spec", class(x))
+  )
+}
