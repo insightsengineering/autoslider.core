a635a6aa0475cef8bc5bb5007e1e685861b86d56
yolandazzz13 <98232916+yolandazzz13@users.noreply.github.com>
2025-07-14 21:25:38 -0400

    added unittests (#79)
    Co-authored-by: yolandazzz13 <yufeng.zhou.yz1@roche.com>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/testthat/test-template.R
+++ tests/testthat/test-template.R
@@ -1,9 +1,3 @@
-library(testthat)
-# library(rprojroot)
-
-# Define the project root and test path
-
-
 test_path <- tempdir()
 
 # test `list_all_templates` -----
@@ -18,7 +12,6 @@ test_that("list_all_templates test 1: returns all available templates", {
   actual <- basename(list_all_templates())
   expect_setequal(actual, expected)
 })
-
 
 
 test_that("use_template test 2: saving when no path is specified", {
@@ -37,9 +30,6 @@ test_that("use_template test 2: saving when no path is specified", {
       open = FALSE
     )
   )
-
-  # Clean up
-
 
 
 
@@ -85,10 +75,43 @@ test_that("use_template test 4: expected errors", {
       open = FALSE
     )
   )
+  expect_error(
+    use_template(
+      template = "ttt", # invalid template
+      function_name = "tryout",
+      save_path = test_path,
+      open = FALSE
+    )
+  )
+})
+
+test_that("use_template test 5: when template is contained in autoslideR", {
+  expect_true(
+    use_template(
+      template = "t_dm_slide",
+      function_name = "tryout",
+      overwrite = TRUE,
+      open = FALSE,
+      package = "autoslideR"
+    )
+  )
+})
+
+# test `use_template` error handling -----
+test_that("use_template test 6: invalid template", {
+  expect_error(
+    use_template(
+      template = "ttt",
+      function_name = "tryout",
+      save_path = test_path,
+      open = FALSE,
+      package = "autoslideR"
+    )
+  )
 })
 
 
-test_that("use_template test 5: all templates", {
+test_that("use_template test 7: all templates", {
   all_templates <- basename(list_all_templates())
   for (template in all_templates) {
     expect_true(
--- tests/testthat/test_ai.R
+++ tests/testthat/test_ai.R
@@ -35,10 +35,64 @@ test_that("Listing print correctly", {
   #   model = "deepseek-chat"
   # )
   outputs <- adding_ai_footnotes(outputs, prompt_list,
-    platform = "galileo",
-    base_url = "https://us.aigw.galileo.roche.com/v1",
-    api_key = get_portkey_key("~/autoslider.core/PORTKEY_KEY"),
-    model = "us.anthropic.claude-3-7-sonnet-20250219-v1:0"
+                                 platform = "galileo",
+                                 base_url = "https://us.aigw.galileo.roche.com/v1",
+                                 api_key = get_portkey_key("~/autoslider.core/PORTKEY_KEY"),
+                                 model = "us.anthropic.claude-3-7-sonnet-20250219-v1:0"
+  )
+  output_dir <- tempdir()
+  testthat::expect_output({
+    outputs %>%
+      generate_slides(outfile = paste0(output_dir, "ai_srep.pptx"), t_cpp = 250, t_lpp = 20)
+  })
+
+  testthat::expect_no_error({
+    outputs %>%
+      save_outputs(outfolder = output_dir)
+  })
+})
+
+
+test_that("using ollama", {
+  testthat::skip_if_not_installed("filters")
+  testthat::skip_if_not_installed("ellmer")
+  testthat::skip_on_ci()
+  testthat::skip_on_cran()
+
+  # skip_if_too_deep(1)
+  filters::load_filters(file.path(
+    system.file(package = "autoslider.core"),
+    "filters.yml"
+  ), overwrite = TRUE)
+
+  spec_file <- file.path(system.file(package = "autoslider.core"), "spec.yml")
+
+  full_spec <- spec_file %>%
+    read_spec()
+
+  outputs <- full_spec %>%
+    filter_spec(., program %in% c(
+      "t_dm_slide"
+    )) %>%
+    generate_outputs(datasets = testdata) %>%
+    decorate_outputs(
+      version_label = NULL,
+      for_test = TRUE
+    )
+  prompt_list <- get_prompt_list(filename = file.path(
+    system.file(package = "autoslider.core"),
+    "prompt.yml"
+  ))
+  # outputs <- adding_ai_footnotes(outputs, prompt_list,
+  #   platform = "deepseek",
+  #   base_url = "https://api.deepseek.com",
+  #   api_key = get_deepseek_key("~/autoslider.core/DEEPSEEK_KEY"),
+  #   model = "deepseek-chat"
+  # )
+  outputs <- adding_ai_footnotes(outputs, prompt_list,
+                                 platform = "ollama",
+                                 base_url = "http://host.docker.internal:11434",
+                                 model = "deepseek-r1:1.5b"
   )
   output_dir <- tempdir()
   testthat::expect_output({
